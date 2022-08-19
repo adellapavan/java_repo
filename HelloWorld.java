4058960ffd91b7faba0d1fec5b3f36482c65c0ca
adellapavan <109579108+adellapavan@users.noreply.github.com>
2022-08-19 20:04:51 +0530

    webhooks1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Compiling this class using jenkins Poll SCM building methodology");
-
+    System.out.println("Build methodology using Jenkins-webhooks");
   }
 }
