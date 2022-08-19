26e1f77d4783f4bdc9528cc6e76895ca4e2db0e5
adellapavan <109579108+adellapavan@users.noreply.github.com>
2022-08-19 19:51:52 +0530

    poll scm2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
     System.out.println("Compiling this class using jenkins Poll SCM building methodology");
 
   }
