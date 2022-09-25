714fa7883e13957a9f48d099b09e25f4f5897f9f
adellapavan <109579108+adellapavan@users.noreply.github.com>
2022-09-25 19:05:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Compiling this class using jenkins Poll SCM building methodology");
     System.out.println("Build methodology using Jenkins-webhooks");
+    System.out.println("Changes part of docker");
   }
 }
